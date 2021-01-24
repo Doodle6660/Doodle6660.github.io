17955ca5041ad7a723047ab17c571d55f28bd6a6
Doodle6660 <ziyadzidane69@gmail.com>
2021-01-24 20:02:00 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
-yarn run build
+npm run build
+
 # navigate into the build output directory
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Doodle6660/hello-world.git master
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:Doodle6660/Doodle6660.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Doodle6660/Doodle6660.github.io.git master:gh-pages
+
 cd -

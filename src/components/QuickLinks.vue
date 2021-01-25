da33afec516957a2d1b7947bbce09e7def6a5dce
Doodle6660 <ziyadzidane69@gmail.com>
2021-01-25 21:10:36 +0700

    add new section

--- src/components/QuickLinks.vue
+++ src/components/QuickLinks.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="ml-4 xl:ml-8">
     <h1 class="text-white text-xl font-semibold">Quick Links</h1>
+    <!-- Section 1 -->
+    <div class="flex flex-wrap justify-between w-3/4 xl:w-1/2">
+      <div class="w-1/2">
+        <a
+          href="#"
+          class="bg-primary text-white font-semibold w-32 py-2 xl:py-3 block text-center rounded rounded-tl-3xl rounded-br-3xl"
+        >
+          Biography
+        </a>
+      </div>
+      <div class="w-1/2">
+        <a
+          href="#"
+          class="bg-primary text-white font-semibold w-32 py-2 xl:py-3 block text-center rounded rounded-tl-3xl rounded-br-3xl"
+        >
+          Works
+        </a>
+      </div>
+      <div class="w-1/2">
+        <a
+          href="#"
+          class="bg-primary text-white font-semibold w-32 py-2 xl:py-3 block text-center rounded rounded-tl-3xl rounded-br-3xl"
+        >
+          Art
+        </a>
+      </div>
+    </div>
+
+    <!-- Section 2 -->
     <div class="flex flex-wrap justify-between w-3/4 xl:w-1/2">
       <div class="w-1/2">
         <a
